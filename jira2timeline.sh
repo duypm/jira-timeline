e145cc829bc8f2150f2ad0315758d34bf1ada882
Duy Pham <duypham@qasymphony.com>
2016-08-19 16:14:05 +0700

    Read jq filter from a file with pretty format

--- jira2timeline.sh
+++ jira2timeline.sh
@@ -3,11 +3,13 @@
 # export your JIRA_BASE64 authentication in this file so it won't be tracked by git
 source ./jira64.sh
 
-curl -D- -X GET \
+curl -s -X GET \
   -H "Authorization: Basic $JIRA_BASE64" \
   -H "Content-Type: application/json" \
   "https://agile.qasymphony.com/rest/api/2/project/QTE/versions" \
   \
-| jq '{events: [ .[] | { text: .name, start_date: { year: .releaseDate | strptime("%Y-%m-%d") | .[0], month: .releaseDate | strptime("%Y-%m-%d") | .[1], day: .releaseDate | strptime("%Y-%m-%d") | .[2] } }]}' \
-  \
-| tee timeline.json
+> qte.json
+
+cat qte.json \
+  | jq -f jq_filter \
+  > timeline.json
